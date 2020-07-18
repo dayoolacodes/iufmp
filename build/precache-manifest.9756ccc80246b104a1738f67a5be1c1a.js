a8292dd722edbde8cfd08280a43c40dda8863849
dayoola <ayodayo1@gmail.com>
2020-07-18 23:37:58 +0100

    wip

---
+++ build/precache-manifest.9756ccc80246b104a1738f67a5be1c1a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981992ad691c5020251e43f3ebdd5986",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29bdea4bee2b68fc70c9",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3d4daa8019fc485560ae",
+    "url": "/static/css/main.f1cd792c.chunk.css"
+  },
+  {
+    "revision": "29bdea4bee2b68fc70c9",
+    "url": "/static/js/2.e38b945c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e38b945c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4daa8019fc485560ae",
+    "url": "/static/js/main.24d7fac4.chunk.js"
+  },
+  {
+    "revision": "d64a3502dc8ac006d2d4",
+    "url": "/static/js/runtime-main.8deac8b5.js"
+  },
+  {
+    "revision": "243e776f58f729332887c6db2e317750",
+    "url": "/static/media/ibadan.243e776f.png"
+  },
+  {
+    "revision": "0ca5fb933937aba74a11c070668226d9",
+    "url": "/static/media/ibadan2.0ca5fb93.jpg"
+  },
+  {
+    "revision": "7a169b84051f4757d25e47aaac49b4e1",
+    "url": "/static/media/ibadan3.7a169b84.PNG"
+  },
+  {
+    "revision": "c3d9e2b7df5cd66dc16133276c21de41",
+    "url": "/static/media/ibadan4.c3d9e2b7.PNG"
+  },
+  {
+    "revision": "81284ab59606d8559219ee2f07b5f53b",
+    "url": "/static/media/ibadan5.81284ab5.PNG"
+  },
+  {
+    "revision": "c4f77520ce88a956cea80a9c725fd210",
+    "url": "/static/media/ibadan6.c4f77520.PNG"
+  }
+]);
